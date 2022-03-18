28b90aeb7443339cf486032a5eb16a985c7bf22a
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-03-18 12:54:14 -0400

    Load Roboto font for MUI css framework

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WebFont from "webfontloader";
+WebFont.load({google: {families: ["Roboto:300,400,500"]}});
 
 ReactDOM.render(
   <React.StrictMode>
