a2b0f2590328313ca4989469383a9a45e0d96898
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-20 19:11:42 +0000

    Add rendering user email when logged in

--- front-end/src/components/NavbarMenu.js
+++ front-end/src/components/NavbarMenu.js
@@ -36,13 +36,14 @@ export default function NavbarMenu (props) {
         console.log(err)
       })
   }
-
+  
   return (
     <Box sx={{ width: '100%' }}>
       <Tabs value={value} onChange={handleChange} aria-label='nav tabs example'>
         {!user && <LinkTab label='About' onClick={() => setMode(ABOUT)} />}
         {!cookies["user"] && <LinkTab label='Login' onClick={() => setMode(LOGIN)} />}
         {!cookies["user"] && <LinkTab label='Sign Up' onClick={() => setMode(REGISTER)} />}
+        {user && <span>Logged in as: {user.email}</span>}
         {cookies["user"] && <LinkTab label='Logout' onClick={logOut} />}
       </Tabs>
     </Box>
