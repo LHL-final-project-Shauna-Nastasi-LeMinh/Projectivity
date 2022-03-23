c6fdb0d8b806e96413f20dbe5b885c7dd8f0941b
Shauna Empringham <shaunathedead86@gmail.com>
2022-03-22 21:08:43 -0400

    adding style to RegistrationForm

--- front-end/src/components/Forms/ConfirmDeleteForm.js
+++ front-end/src/components/Forms/ConfirmDeleteForm.js
@@ -9,6 +9,7 @@ import FormHelperText from '@mui/material/FormHelperText'
 import Input from '@mui/material/Input'
 import InputLabel from '@mui/material/InputLabel'
 import { PROJECT_VIEW } from '../constants/Modes'
+import Card from '@mui/material/Card'
 
 export default function ConfirmDeletedForm (props) {
   const [message, setMessage] = useState('')
@@ -48,6 +49,18 @@ export default function ConfirmDeletedForm (props) {
     }
   }
 
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'palette.primary.main',
+    border: '2px solid palette.secondary.main',
+    boxShadow: 24,
+    p: 4
+  }
+
   return (
     <Modal
       open={open}
@@ -55,40 +68,42 @@ export default function ConfirmDeletedForm (props) {
       aria-labelledby='modal-login-form'
       aria-describedby='modal-modal-login-form'
 		>
-      <FormControl>
-        <Box
-          component='form'
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' }
-          }}
-          noValidate
-          autoComplete='off'
-				>
-          <Typography variant='h4'>Project Deletion</Typography>
-          <Typography variant='h6' sx={{ color: 'palette.warning.main' }}>
-						You are about to delete the following project: {currentProject}
-          </Typography>
-          <Typography variant='h6' sx={{ color: 'palette.error.main' }}>
-            {message}
-          </Typography>
-          <InputLabel htmlFor='component-error'>
-						Type DELETE here before confirming
-					</InputLabel>
-          <Input
-            id='component-error'
-            value={confirm}
-            onChange={handleChange}
-            aria-describedby='component-error-text'
-					/>
-          <FormHelperText id='component-error-text'>Error</FormHelperText>
-          <Button variant='outlined' onClick={delete_confirmed}>
-						Confirm Delete
-					</Button>
-          <Button variant='outlined' onClick={() => setOpen('')}>
-						Cancel
-					</Button>
-        </Box>
-      </FormControl>
+      <Card sx={style}>
+        <FormControl>
+          <Box
+            component='form'
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' }
+            }}
+            noValidate
+            autoComplete='off'
+					>
+            <Typography variant='h4'>Project Deletion</Typography>
+            <Typography variant='h6' sx={{ color: 'palette.warning.main' }}>
+							You are about to delete the following project: {currentProject}
+            </Typography>
+            <Typography variant='h6' sx={{ color: 'palette.error.main' }}>
+              {message}
+            </Typography>
+            <InputLabel htmlFor='component-error'>
+							Type DELETE here before confirming
+						</InputLabel>
+            <Input
+              id='component-error'
+              value={confirm}
+              onChange={handleChange}
+              aria-describedby='component-error-text'
+						/>
+            <FormHelperText id='component-error-text'>Error</FormHelperText>
+            <Button variant='outlined' onClick={delete_confirmed}>
+							Confirm Delete
+						</Button>
+            <Button variant='outlined' onClick={() => setOpen('')}>
+							Cancel
+						</Button>
+          </Box>
+        </FormControl>
+      </Card>
     </Modal>
   )
 }
