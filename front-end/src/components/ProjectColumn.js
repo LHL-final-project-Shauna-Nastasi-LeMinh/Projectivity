77bf3ff0105d23ed33e6c54ef9b242f5b44a8c70
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-03-24 01:13:37 -0400

    fix dnd bug resulted from merging

--- front-end/src/components/ProjectColumn.js
+++ front-end/src/components/ProjectColumn.js
@@ -181,6 +181,7 @@ export default function ProjectColumn (props) {
 								/>}
             </DialogContent>
             <DialogActions>
+              
               <Button onClick={handleColumnActions}>
                 {dialogContent.confirmLabel}
               </Button>
@@ -194,9 +195,9 @@ export default function ProjectColumn (props) {
               <List
                 {...provided.droppableProps}
                 ref={provided.innerRef}
-                isdraggingover={snapshot.isdraggingover}
+                isdraggingover={snapshot.isDraggingOver}
                 sx={{
-                  backgroundColor: snapshot.isdraggingover
+                  backgroundColor: snapshot.isDraggingOver
 										? 'skyblue'
 										: 'inherit',
                   transition: 'background-color 1s ease'
