8a90382ecb73659bcef8ee13c5920919655f2694
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-03-25 17:56:45 -0400

    Web Socket for dragging columns

--- back-end-with-Sequelize/routes/columns.js
+++ back-end-with-Sequelize/routes/columns.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const COLUMN_CHANNEL = "Column_Channel"
+const COLUMN_MOVE_EVENT = "Column_Move_Event"
 
-module.exports = (sequelizeModels) => {
+module.exports = (sequelizeModels, pusher) => {
 
   Column = sequelizeModels.Column;
+  Ticket = sequelizeModels.Ticket;
+
   router.post('/new', async (req, res) => {
     try {
       const {name, project_id} = req.body
@@ -36,14 +40,28 @@ module.exports = (sequelizeModels) => {
   })
 
   router.post('/reodering', async (req, res) => {
+    const project_id = req.body.project_id;
+    const orderingObject = req.body.ordering
     try {
-      for (const columnId in req.body) {
+      for (const columnId in orderingObject) {
         await Column.update(
-          { ordering_index: req.body[columnId] },
+          { ordering_index: orderingObject[columnId] },
           { where: { id: columnId } }
         )
-
       }
+      // WebSocket broadcast
+      const columns = await Columns.findAll({
+        where: { project_id: project_id},
+        include: [
+          {
+            model: sequelizeModels.Ticket
+          }
+        ],
+        order: [['ordering_index', 'ASC']]
+      })
+      pusher.trigger(COLUMN_CHANNEL, COLUMN_MOVE_EVENT, columns);
+      // End WebSocket
+
       return res.json({message: "column reordered successfully"});
     } catch (err) {
       console.log(err)
