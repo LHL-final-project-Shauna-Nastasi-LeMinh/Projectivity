7a5056d3ad9b4a4ac9e85b03ce6d1c43db52067c
Shauna Empringham <shaunathedead86@gmail.com>
2022-03-25 22:06:46 -0400

    adding more styling changes

--- back-end-with-Sequelize/routes/tickets.js
+++ back-end-with-Sequelize/routes/tickets.js
@@ -1,40 +1,40 @@
-const express = require('express')
-const column = require('../models/column')
-const users = require('./users')
-const router = express.Router()
+const express = require("express");
+const column = require("../models/column");
+const users = require("./users");
+const router = express.Router();
 
-module.exports = sequelizeModels => {
-  projectTickets = sequelizeModels.ProjectAssignment
-  Ticket = sequelizeModels.Ticket
+module.exports = (sequelizeModels) => {
+  projectTickets = sequelizeModels.ProjectAssignment;
+  Ticket = sequelizeModels.Ticket;
 
-  router.get('/:employee_id', async (req, res) => {
+  router.get("/:employee_id", async (req, res) => {
     try {
       const project_assignments = await Project_Assignments.findAll({
         where: { employee_id: req.params.employee_id }, // selectedProject
         include: [
           {
-            model: sequelizeModels.Project
-          }
-        ]
-      })
+            model: sequelizeModels.Project,
+          },
+        ],
+      });
 
       const columns = await column.findAll({
         where: { project_id: project_assignments[0].project_id },
         include: [
           {
-            model: sequelizeModels.Ticket
-          }
-        ]
-      })
+            model: sequelizeModels.Ticket,
+          },
+        ],
+      });
 
-      return res.json(columns)
+      return res.json(columns);
     } catch (err) {
-      console.log(err)
-      return res.status(500).json(err)
+      console.log(err);
+      return res.status(500).json(err);
     }
-  })
+  });
 
-  router.post('/new', async (req, res) => {
+  router.post("/new", async (req, res) => {
     try {
       const {
         title,
@@ -44,8 +44,8 @@ module.exports = sequelizeModels => {
         severity,
         priority,
         type,
-        milestone
-      } = req.body
+        milestone,
+      } = req.body;
 
       const newTicket = await Tickets.create({
         title,
@@ -55,65 +55,68 @@ module.exports = sequelizeModels => {
         severity,
         priority,
         type,
-        milestone
-      })
+        milestone,
+      });
 
       return res.json(newTicket);
     } catch (err) {
-      console.log(err, "foo")
-      return res.status(500).json(err)
+      console.log(err, "foo");
+      return res.status(500).json(err);
     }
-  })
+  });
 
-  router.delete('/:ticket_id', async (req, res) => {
+  router.delete("/:ticket_id", async (req, res) => {
     try {
-      const ticket_id = req.params.ticket_id
+      const ticket_id = req.params.ticket_id;
       Tickets.destroy({
         where: {
-          id: ticket_id
-        }
-      })
+          id: ticket_id,
+        },
+      });
 
-      return res.json('success!')
+      return res.json("success!");
     } catch (err) {
-      console.log(err)
-      return res.status(500).json(err)
+      console.log(err);
+      return res.status(500).json(err);
     }
-  })
+  });
 
-  router.post('/:ticket_id', async (req, res) => {
+  router.post("/:ticket_id", async (req, res) => {
     try {
-      const {id, title, description, severity, priority, type, milestone} = req.body
+      const { id, title, description, severity, priority, type, milestone } =
+        req.body;
       await Tickets.update(
-        {title, description, severity, priority, type, milestone},
+        { title, description, severity, priority, type, milestone },
         {
-        where: {
-          id: id
+          where: {
+            id: id,
+          },
         }
-      })
+      );
       const updatedTicket = await Tickets.findAll({
-        where: { id: id } })
-      console.log("backend", updatedTicket)
+        where: { id: id },
+      });
+      console.log("backend", updatedTicket);
 
-      return res.json(updatedTicket)
+      return res.json(updatedTicket);
     } catch (err) {
-      console.log(err)
-      return res.status(500).json(err)
+      console.log(err);
+      return res.status(500).json(err);
     }
-  })
+  });
 
-  router.post('/updateColumn', async (req, res) => {
+  router.post("/updateColumn", async (req, res) => {
     try {
-      const {ticketId, newColumnId} = req.body
+      const { ticketId, newColumnId } = req.body;
       await Ticket.update(
         { column_id: newColumnId },
         { where: { id: ticketId } }
-      )
+      );
       return res.json({ message: "Column updated for ticket" });
     } catch (err) {
-      console.log(err)
-      return res.status(500).json(err)
+      console.log(err);
+      return res.status(500).json(err);
     }
-  })
-  return router
-}
+  });
+  return router;
+};
