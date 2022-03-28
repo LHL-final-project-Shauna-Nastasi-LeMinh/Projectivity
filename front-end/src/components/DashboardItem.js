4395c6733b153f563fe36108357e0f9142eb179c
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-03-27 23:07:11 -0400

    Fix bug: not close 3dots menu when opening edit/delete project modal

--- front-end/src/components/DashboardItem.js
+++ front-end/src/components/DashboardItem.js
@@ -1,126 +1,132 @@
-import React, { useEffect, useState } from 'react';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { ADDPROJECT } from './constants/Modes';
-import axios from 'axios';
-import { PROJECT_VIEW, DELETE_PROJECT_FORM } from './constants/Modes';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import { MANAGER_LEVEL } from './constants/AccessLevel';
-import Box from '@mui/material/Box';
-import Menu from '@mui/material/Menu';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import FolderIcon from '@mui/icons-material/Folder';
-import { theme } from './Theme';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useEffect, useState } from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { ADDPROJECT } from "./constants/Modes";
+import axios from "axios";
+import { PROJECT_VIEW, DELETE_PROJECT_FORM } from "./constants/Modes";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { MANAGER_LEVEL } from "./constants/AccessLevel";
+import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import FolderIcon from "@mui/icons-material/Folder";
+import { theme } from "./Theme";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function DashboardItem(props) {
-	const {
-		key,
-		project,
-		currentProject,
-		setCurrentProject,
-		dashItemProject,
-		viewMode,
-		setViewMode,
-		value,
-		listIndex,
-		selectProject,
-		loadForm,
-		user,
-		modals,
-		openModals,
-		closeModals,
-		selectedIndex,
-		setSelectedIndex,
-		userData,
-		selectModal
-	} = props;
-	const [anchorEl, setAnchorEl] = useState(null);
-	const open = Boolean(anchorEl);
+  const {
+    key,
+    project,
+    currentProject,
+    setCurrentProject,
+    dashItemProject,
+    viewMode,
+    setViewMode,
+    value,
+    listIndex,
+    selectProject,
+    loadForm,
+    user,
+    modals,
+    openModals,
+    closeModals,
+    selectedIndex,
+    setSelectedIndex,
+    userData,
+    selectModal,
+  } = props;
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-	function handleClick(target, event, index) {
-		console.log(target, open, anchorEl);
-		if (open === false) {
-			if (target === 'menu') {
-				setAnchorEl(event.currentTarget);
-				event.stopPropagation();
-			} else {
-				selectProject(index);
-				setSelectedIndex(index);
-			}
-		}
-	}
+  function handleClick(target, event, index) {
+    console.log(target, open, anchorEl);
+    if (open === false) {
+      if (target === "menu") {
+        setAnchorEl(event.currentTarget);
+        event.stopPropagation();
+      } else {
+        selectProject(index);
+        setSelectedIndex(index);
+      }
+    }
+  }
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	return (
-		<ListItemButton
-			selected={selectedIndex === listIndex}
-			onClick={(event) => {
-				handleClick('listItem', event, listIndex);
-			}}
-			sx={{ px: 2 }}
-		>
-			<FolderIcon sx={{ mx: 1, color: 'primary.light', px: 2 }} />
-			<ListItemText key={key} primary={value} />
-			<IconButton
-				onClick={(event) => {
-					handleClick('menu', event, listIndex);
-				}}
-				id="demo-positioned-button"
-				aria-controls={open ? 'demo-positioned-menu' : undefined}
-				aria-haspopup="true"
-				aria-expanded={open ? 'true' : undefined}
-				sx={{ px: 2 }}
-			>
-				<MenuIcon fontSize="medium" sx={{ color: 'background.default' }} />
-			</IconButton>
-			<Menu
-				id="demo-positioned-menu"
-				aria-labelledby="demo-positioned-button"
-				anchorEl={anchorEl}
-				open={open}
-				onClose={handleClose}
-				color="primary.main"
-				anchorOrigin={{
-					vertical: 'bottom',
-					horizontal: 'left'
-				}}
-				transformOrigin={{
-					vertical: 'top',
-					horizontal: 'left'
-				}}
-			>
-				<MenuItem
-					sx={{ '&:hover': { backgroundColor: 'secondary.light' } }}
-					onClick={() => selectModal('editProjectForm', dashItemProject)}
-				>
-					<EditIcon fontSize="small" />
-					Edit Project
-				</MenuItem>
-				<MenuItem
-					sx={{ '&:hover': { backgroundColor: 'secondary.light' } }}
-					onClick={() => selectModal('deleteProjectForm', dashItemProject)}
-				>
-					<DeleteIcon fontSize="small" />
-					Delete Project
-				</MenuItem>
-			</Menu>
-		</ListItemButton>
-	);
+  return (
+    <ListItemButton
+      selected={selectedIndex === listIndex}
+      onClick={event => {
+        handleClick("listItem", event, listIndex);
+      }}
+      sx={{ px: 2 }}
+    >
+      <FolderIcon sx={{ mx: 1, color: "primary.light", px: 2 }} />
+      <ListItemText key={key} primary={value} />
+      <IconButton
+        onClick={event => {
+          handleClick("menu", event, listIndex);
+        }}
+        id="demo-positioned-button"
+        aria-controls={open ? "demo-positioned-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        sx={{ px: 2 }}
+      >
+        <MenuIcon fontSize="medium" sx={{ color: "background.default" }} />
+      </IconButton>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        color="primary.main"
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem
+          sx={{ "&:hover": { backgroundColor: "secondary.light" } }}
+          onClick={() => {
+            selectModal("editProjectForm", dashItemProject);
+            handleClose();
+          }}
+        >
+          <EditIcon fontSize="small" />
+          Edit Project
+        </MenuItem>
+        <MenuItem
+          sx={{ "&:hover": { backgroundColor: "secondary.light" } }}
+          onClick={() => {
+            selectModal("deleteProjectForm", dashItemProject);
+            handleClose();
+          }}
+        >
+          <DeleteIcon fontSize="small" />
+          Delete Project
+        </MenuItem>
+      </Menu>
+    </ListItemButton>
+  );
 }
--- front-end/src/components/ProjectTicket.js
+++ front-end/src/components/ProjectTicket.js
@@ -1,187 +1,179 @@
-import * as React from 'react';
-import { useState } from 'react';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import IconButton from '@mui/material/IconButton';
-import { Box, Chip, Typography, Popover, Avatar, Stack } from '@mui/material'
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import Fade from '@mui/material/Fade';
-import RemoveTicket from './Forms/RemoveTicket';
-import ShowTicketDetails from './Forms/ShowTicketDetails';
-import NewTicketForm from './Forms/NewTicketForm';
-import TicketHistory from './Forms/TicketHistory';
-import AssignTicket from './Forms/AssignTicket';
-
+import * as React from "react";
+import { useState } from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import { Box, Chip, Typography, Popover, Avatar, Stack } from "@mui/material";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import Fade from "@mui/material/Fade";
+import RemoveTicket from "./Forms/RemoveTicket";
+import ShowTicketDetails from "./Forms/ShowTicketDetails";
+import NewTicketForm from "./Forms/NewTicketForm";
+import TicketHistory from "./Forms/TicketHistory";
+import AssignTicket from "./Forms/AssignTicket";
 
 import useEmployeesData from "../hooks/useEmployeesData";
 
-import { BlockRounded } from '@mui/icons-material';
-import { padding, palette, textAlign } from '@mui/system';
+import { BlockRounded } from "@mui/icons-material";
+import { padding, palette, textAlign } from "@mui/system";
 
 import {
-	SHOW_TICKET_DETAILS,
-	EDIT_TICKET,
-	REMOVE_TICKET,
-	ADD_TICKET,
-	TICKET_HISTORY,
-
-} from './constants/Modes';
-import { MANAGER_LEVEL } from './constants/AccessLevel';
-import { modalClasses } from '@mui/material';
+  SHOW_TICKET_DETAILS,
+  EDIT_TICKET,
+  REMOVE_TICKET,
+  ADD_TICKET,
+  TICKET_HISTORY,
+} from "./constants/Modes";
+import { MANAGER_LEVEL } from "./constants/AccessLevel";
+import { modalClasses } from "@mui/material";
 
 export default function ProjectTicket(props) {
-	const {
-		title,
-		value,
-		ticketId,
-		setViewMode,
-		setOpen,
-		tickets,
-		setTickets,
-		user,
-		currentColumn,
-		ticket,
-		setCurrentColumn,
-		currentProject,
-		userData,
-		setUserData,
-		editTicket,
-		setEditTicket
-	} = props;
+  const {
+    title,
+    value,
+    ticketId,
+    setViewMode,
+    setOpen,
+    tickets,
+    setTickets,
+    user,
+    currentColumn,
+    ticket,
+    setCurrentColumn,
+    currentProject,
+    userData,
+    setUserData,
+    editTicket,
+    setEditTicket,
+  } = props;
 
-	const [checked, setChecked] = React.useState([1]);
-	const [dialogOpen, setDialogOpen] = useState(false);
-	const [currentTicket, setCurrentTicket] = useState();
+  const [checked, setChecked] = React.useState([1]);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [currentTicket, setCurrentTicket] = useState();
 
   // handle opening and closing of MoreVertIcon
   const [anchorEl, setAnchorEl] = useState(null);
-	const [anchorPop, setAnchorPop] = useState(null);
-	const openPop = Boolean(anchorPop);
+  const [anchorPop, setAnchorPop] = useState(null);
+  const openPop = Boolean(anchorPop);
   const openMenu = Boolean(anchorEl);
-  const handleClick = (event) => {
+  const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
 
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
 
-	const closeMenu = () => {
-		setAnchorEl(null);
-	};
+  // for popover
+  const id = openPop ? "simple-popover" : undefined;
 
-	// for popover
-	const id = openPop ? 'simple-popover' : undefined;
+  const openPopover = event => {
+    setCurrentTicket(ticketId);
+    setAnchorPop(event.currentTarget);
+  };
 
-	const openPopover = (event) => {
-		setCurrentTicket(ticketId)
-		setAnchorPop(event.currentTarget);
-	}
+  const closePopover = () => {
+    setAnchorPop(false);
+  };
 
-	const closePopover = () => {
-		setAnchorPop(false);
-	}
+  const handleDialogOpening = evt => {
+    console.log(evt.target.id);
+    console.log(evt.currentTarget);
 
+    if (evt.target.id === "edit") {
+      // openModals('newTicketForm');
+      setEditTicket(true);
+      setDialogOpen(EDIT_TICKET);
+      setCurrentTicket(ticketId);
+    }
 
-	const handleDialogOpening = (evt) => {
-		console.log(evt.target.id);
-		console.log(evt.currentTarget)
+    if (evt.target.id === "details") {
+      setDialogOpen(SHOW_TICKET_DETAILS);
+      console.log(dialogOpen);
+    }
 
-		if (evt.target.id === 'edit') {
-			// openModals('newTicketForm');
-			setEditTicket(true);
-			setDialogOpen(EDIT_TICKET);
-			setCurrentTicket(ticketId);
-		}
+    if (evt.target.id === "remove") {
+      setDialogOpen(REMOVE_TICKET);
+      console.log(dialogOpen);
+    }
 
-		if (evt.target.id === 'details') {
-			setDialogOpen(SHOW_TICKET_DETAILS);
-			console.log(dialogOpen);
-		}
+    if (evt.target.id === "history") {
+      setDialogOpen(TICKET_HISTORY);
+      console.log(dialogOpen);
+    }
 
-		if (evt.target.id === 'remove') {
-			setDialogOpen(REMOVE_TICKET);
-			console.log(dialogOpen);
-		}
+    closeMenu();
+  };
 
-		if (evt.target.id === 'history') {
-			setDialogOpen(TICKET_HISTORY);
-			console.log(dialogOpen);
-		}
+  //
 
-		closeMenu();
-	};
+  const handleToggle = value => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
 
-	//
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
 
-	const handleToggle = (value) => () => {
-		const currentIndex = checked.indexOf(value);
-		const newChecked = [...checked];
+    setChecked(newChecked);
+  };
 
-		if (currentIndex === -1) {
-			newChecked.push(value);
-		} else {
-			newChecked.splice(currentIndex, 1);
-		}
+  const choosePriorityColor = priority => {
+    const color = {
+      min: "#264653",
+      low: "#2A9D8F",
+      medium: "#ffba08",
+      high: "#f3722c",
+      max: "#f94144",
+    };
 
-		setChecked(newChecked);
-	};
+    switch (priority) {
+      case "Urgent":
+        return color.max;
+        break;
+      case "Essential":
+        return color.high;
+        break;
+      case "Valuable":
+        return color.medium;
+        break;
+      case "Discretionary":
+        return color.low;
+        break;
+      default:
+        return color.min;
+        break;
+    }
+  };
 
-	const choosePriorityColor = (priority) => {
-		const color = {
-			min: '#264653',
-			low: '#2A9D8F',
-			medium: '#ffba08',
-			high: '#f3722c',
-			max: '#f94144'
-		};
+  const employee = useEmployeesData(ticket.owner_id, tickets);
 
-		switch (priority) {
-			case 'Urgent':
-				return color.max;
-				break;
-			case 'Essential':
-				return color.high;
-				break;
-			case 'Valuable':
-				return color.medium;
-				break;
-			case 'Discretionary':
-				return color.low;
-				break;
-			default:
-				return color.min;
-				break;
-		}
-	};
-
-	
-	
-
-	const employee = useEmployeesData(ticket.owner_id, tickets)
-
-	const isAvatar = () => {
-
-		return 
-	}
+  const isAvatar = () => {
+    return;
+  };
 
   return (
-	
-	  
-    <ListItem sx={{  display: "block"}}>
-			<Stack direction="row" spacing={1}>
-				{ticket.priority &&
-					<Chip
-          pl="2"
-          label={ticket.priority}
-         
-					style={{backgroundColor: choosePriorityColor(ticket.priority), fontWeight:'700'}}
-          size="small"
-        />
-				}
+    <ListItem sx={{ display: "block" }}>
+      <Stack direction="row" spacing={1}>
+        {ticket.priority && (
+          <Chip
+            pl="2"
+            label={ticket.priority}
+            style={{
+              backgroundColor: choosePriorityColor(ticket.priority),
+              fontWeight: "700",
+            }}
+            size="small"
+          />
+        )}
 
-				{/* {!employee.avatar.length && Object.keys(employee).length !== 0 && ticket.owner_id &&
+        {/* {!employee.avatar.length && Object.keys(employee).length !== 0 && ticket.owner_id &&
 				<Avatar 
 					sx={{ width: 26, height: 26, borderRadius: 1 }}
 					 >
@@ -190,113 +182,101 @@ export default function ProjectTicket(props) {
 					
         } */}
 
-				{Object.keys(employee).length !== 0 && ticket.owner_id && 
-				  <Avatar 
-					sx={{ width: 26, height: 26, borderRadius: 1}}
-					size={100}
-					alt='JON'
-					src={employee.avatar}>
-						<img alt={`${employee.first_name[0]}${employee.last_name[0]}`}></img>
-						</Avatar>
-        }
-				</Stack>
-        
-        
+        {Object.keys(employee).length !== 0 && ticket.owner_id && (
+          <Avatar
+            sx={{ width: 26, height: 26, borderRadius: 1 }}
+            size={100}
+            alt="JON"
+            src={employee.avatar}
+          >
+            <img
+              alt={`${employee.first_name[0]}${employee.last_name[0]}`}
+            ></img>
+          </Avatar>
+        )}
+      </Stack>
+
       <ListItemButton
         sx={{
-          px: '0',
-          display:"flex", justifyContent:"space-between", alignItems:"center"
+          px: "0",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
-        
-			>
-        
-
-				
-          <div>
-					<Typography sx={{ color: 'background.default' }}>{title}</Typography>
-				{/* <ListItemText primary={title} sx={{ fontSize: 'small' }} /> */}
+      >
+        <div>
+          <Typography sx={{ color: "background.default" }}>{title}</Typography>
+          {/* <ListItemText primary={title} sx={{ fontSize: 'small' }} /> */}
         </div>
-        
-        
-        <div style={{ display: "inherit"}}>
-        <IconButton 
-					sx={{px:"0"}}
-					aria-describedby={id} 
-					variant="contained"
-					onClick={openPopover}
-				>
-					<PersonAddIcon 
-					sx={{ fontSize: 'small'}}
-					
-					/>
-					</IconButton >
 
-					<Popover
-				    id={id}
-				    open={anchorPop}
-				    anchorEl={anchorPop}
-				    onClose={closePopover}
-						sx={{pb:3}}
-				    anchorOrigin={{
-				    vertical: 'bottom',
-				    horizontal: 'right',
-						
-				  }}
-				>
-				  
-						<Box
-						sx={{
-						border: 1,
-						borderColor: 'grey.500', 
-						backgroundColor: 'primary.main',
-						color: 'background.default',
-            mb: 2, 
-            p: 2,
-						textAlign: 'center',
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center'
+        <div style={{ display: "inherit" }}>
+          <IconButton
+            sx={{ px: "0" }}
+            aria-describedby={id}
+            variant="contained"
+            onClick={openPopover}
+          >
+            <PersonAddIcon sx={{ fontSize: "small" }} />
+          </IconButton>
 
-					}}
-				>
-					<Typography variant="p" align="center">
-					<PersonAddIcon 
-					 fontSize='small'
-						color="secondary"
-					
-					/>
-					</Typography>
-					<Typography variant="p" align="center">
-						Assign Ticket To An Employee
-					</Typography>
-				</Box>
-				<Box sx={{display: 'inline-flex', justifyContent:"center", pb: 3}}>
-						<AssignTicket
-							currentProject={currentProject}
-							ticketId={ticketId}
-							setAnchorPop={setAnchorPop}
-							setTickets={setTickets}
-							tickets={tickets}
-							user={user}
-							title={title}
-						/>
-					</Box>
-					
-				</Popover>
+          <Popover
+            id={id}
+            open={anchorPop}
+            anchorEl={anchorPop}
+            onClose={closePopover}
+            sx={{ pb: 3 }}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <Box
+              sx={{
+                border: 1,
+                borderColor: "grey.500",
+                backgroundColor: "primary.main",
+                color: "background.default",
+                mb: 2,
+                p: 2,
+                textAlign: "center",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="p" align="center">
+                <PersonAddIcon fontSize="small" color="secondary" />
+              </Typography>
+              <Typography variant="p" align="center">
+                Assign Ticket To An Employee
+              </Typography>
+            </Box>
+            <Box
+              sx={{ display: "inline-flex", justifyContent: "center", pb: 3 }}
+            >
+              <AssignTicket
+                currentProject={currentProject}
+                ticketId={ticketId}
+                setAnchorPop={setAnchorPop}
+                setTickets={setTickets}
+                tickets={tickets}
+                user={user}
+                title={title}
+              />
+            </Box>
+          </Popover>
 
+          <IconButton
+            id="fade-button"
+            aria-controls={openMenu ? "fade-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={openMenu ? "true" : undefined}
+            onClick={handleClick}
+            sx={{ px: "0", ml: "1" }}
+          >
+            <MoreVertIcon />
 
-
-        <IconButton
-          id="fade-button"
-          aria-controls={openMenu ? 'fade-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={openMenu ? 'true' : undefined}
-          onClick={handleClick}
-          sx={{px:"0", ml:"1"}}
-        >  
-          <MoreVertIcon />
-
-				 {/* <div style={{ display: 'inherit' }}>
+            {/* <div style={{ display: 'inherit' }}>
 					<IconButton sx={{ px: '0' }}>
 						<PersonAddIcon sx={{ fontSize: 'small' }} />
 					</IconButton>
@@ -309,90 +289,89 @@ export default function ProjectTicket(props) {
 						sx={{ px: '0', ml: '1' }}
 					>
 						<MoreVertIcon /> */}
-					</IconButton> 
+          </IconButton>
 
-					{dialogOpen === SHOW_TICKET_DETAILS && (
-						<ShowTicketDetails
-							tickets={tickets}
-							setTickets={setTickets}
-							ticketId={ticketId}
-							setViewMode={setViewMode}
-							dialogOpen={dialogOpen}
-							setDialogOpen={setDialogOpen}
-						/>
-					)}
-					{dialogOpen === REMOVE_TICKET && (
-						<RemoveTicket
-							user={user}
-							ticket={ticket}
-							tickets={tickets}
-							setTickets={setTickets}
-							ticketId={ticketId}
-							dialogOpen={dialogOpen}
-							setDialogOpen={setDialogOpen}
-							currentProject={currentProject}
-							userData={userData}
-							setCurrentColumn={setCurrentColumn}
-						/>
-					)}
+          {dialogOpen === SHOW_TICKET_DETAILS && (
+            <ShowTicketDetails
+              tickets={tickets}
+              setTickets={setTickets}
+              ticketId={ticketId}
+              setViewMode={setViewMode}
+              dialogOpen={dialogOpen}
+              setDialogOpen={setDialogOpen}
+            />
+          )}
+          {dialogOpen === REMOVE_TICKET && (
+            <RemoveTicket
+              user={user}
+              ticket={ticket}
+              tickets={tickets}
+              setTickets={setTickets}
+              ticketId={ticketId}
+              dialogOpen={dialogOpen}
+              setDialogOpen={setDialogOpen}
+              currentProject={currentProject}
+              userData={userData}
+              setCurrentColumn={setCurrentColumn}
+            />
+          )}
 
-					{dialogOpen === EDIT_TICKET && (
-						<NewTicketForm
-							user={user}
-							currentColumn={currentColumn}
-							tickets={tickets}
-							setTickets={setTickets}
-							dialogOpen={dialogOpen}
-							ticketId={ticketId}
-							setDialogOpen={setDialogOpen}
-							title="Edit Ticket"
-							onsubmitMsg="Edit Ticket"
-							currentTicket={currentTicket}
-							currentProject={currentProject}
-							userData={userData}
-							setUserData={setUserData}
-							editTicket={editTicket}
-						/>
-					)}
+          {dialogOpen === EDIT_TICKET && (
+            <NewTicketForm
+              user={user}
+              currentColumn={currentColumn}
+              tickets={tickets}
+              setTickets={setTickets}
+              dialogOpen={dialogOpen}
+              ticketId={ticketId}
+              setDialogOpen={setDialogOpen}
+              title="Edit Ticket"
+              onsubmitMsg="Edit Ticket"
+              currentTicket={currentTicket}
+              currentProject={currentProject}
+              userData={userData}
+              setUserData={setUserData}
+              editTicket={editTicket}
+            />
+          )}
 
-					{dialogOpen === TICKET_HISTORY && (
-						<TicketHistory
-							tickets={tickets}
-							setTickets={setTickets}
-							ticketId={ticketId}
-							dialogOpen={dialogOpen}
-							setDialogOpen={setDialogOpen}
-						/>
-					)}
+          {dialogOpen === TICKET_HISTORY && (
+            <TicketHistory
+              tickets={tickets}
+              setTickets={setTickets}
+              ticketId={ticketId}
+              dialogOpen={dialogOpen}
+              setDialogOpen={setDialogOpen}
+            />
+          )}
 
-					<Menu
-						id="fade-menu"
-						MenuListProps={{
-							'aria-labelledby': 'fade-button'
-						}}
-						anchorEl={anchorEl}
-						open={openMenu}
-						onClose={closeMenu}
-						TransitionComponent={Fade}
-					>
-						<MenuItem id="details" onClick={(evt) => handleDialogOpening(evt)}>
-							Details
-						</MenuItem>
-						<MenuItem id="edit" onClick={handleDialogOpening}>
-							Edit
-						</MenuItem>
-						{user.access_level == MANAGER_LEVEL && (
-							<MenuItem id="remove" onClick={(evt) => handleDialogOpening(evt)}>
-								Remove
-							</MenuItem>
-						)}
-						<MenuItem id="history" onClick={handleDialogOpening}>
-							History
-						</MenuItem>
-					</Menu>
-				</div>
-				</ListItemButton>
-			
-		</ListItem>
-	);
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              "aria-labelledby": "fade-button",
+            }}
+            anchorEl={anchorEl}
+            open={openMenu}
+            onClose={closeMenu}
+            TransitionComponent={Fade}
+          >
+            <MenuItem id="details" onClick={evt => handleDialogOpening(evt)}>
+              Details
+            </MenuItem>
+            <MenuItem id="edit" onClick={handleDialogOpening}>
+              Edit
+            </MenuItem>
+            {user.access_level == MANAGER_LEVEL && (
+              <MenuItem id="remove" onClick={evt => handleDialogOpening(evt)}>
+                Remove
+              </MenuItem>
+            )}
+            <MenuItem id="history" onClick={handleDialogOpening}>
+              History
+            </MenuItem>
+          </Menu>
+        </div>
+      </ListItemButton>
+    </ListItem>
+  );
 }
