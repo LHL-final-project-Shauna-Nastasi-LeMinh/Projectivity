0da9d8936be49cce12b392f7a7eb1faa53586232
Shauna Empringham <shaunathedead86@gmail.com>
2022-03-29 14:06:31 -0400

    fixing new/edit project forms

--- front-end/src/components/NavbarMenu.js
+++ front-end/src/components/NavbarMenu.js
@@ -249,7 +249,7 @@ export default function NavbarMenu(props) {
 											</Typography>
 										</Box>
 									)}
-									{notifications && (
+									{unreadNotifLength >= 0 && (
 										<NotificationDrawer
 											notifications={notifications}
 											setNotifications={setNotifications}
